627f31d8e425f718383445d06073aeea84004b17
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-01-21 13:14:45 +0100

    clean builds from aux files/folders

--- pkNX.Structures.FlatBuffers/PokeData/PokeDataBattle.cs
+++ pkNX.Structures.FlatBuffers/PokeData/PokeDataBattle.cs
@@ -1,6 +1,4 @@
-using System;
 using System.ComponentModel;
-using System.IO;
 using FlatSharp.Attributes;
 // ReSharper disable UnusedMember.Global
 // ReSharper disable ClassNeverInstantiated.Global
