8e6e8bb9925e438d19f7d656498789bbc5e7dfe3
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-02-28 12:38:10 +0100

    update dependencies

--- Classes/EncounterTera9.cs
+++ Classes/EncounterTera9.cs
@@ -141,7 +141,7 @@ public sealed record EncounterTera9 : EncounterStatic, ITeraRaid9
             return true;
 
         var pi = PersonalTable.SV.GetFormEntry(Species, Form);
-        var param = new GenerateParam9(Species, pi.Gender, FlawlessIVCount, 1, 0, 0, 0, Ability, Shiny);
+        var param = new GenerateParam9(Species, pi.Gender, FlawlessIVCount, 1, 0, 0, 0, 0, Ability, Shiny);
         if (!Encounter9RNG.IsMatch(pk, param, seed))
             return true;
         return base.IsMatchPartial(pk);
@@ -162,7 +162,7 @@ public sealed record EncounterTera9 : EncounterStatic, ITeraRaid9
         const byte undefinedSize = 0;
         var pi = PersonalTable.SV.GetFormEntry(Species, Form);
         var param = new GenerateParam9(Species, pi.Gender, FlawlessIVCount, rollCount,
-            undefinedSize, undefinedSize, undefinedSize,
+            undefinedSize, undefinedSize, undefinedSize, undefinedSize,
             Ability, Shiny);
 
         var init = Util.Rand.Rand64();
