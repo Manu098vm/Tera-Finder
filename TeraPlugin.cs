9bb19822136de9ada5b091399fc506b403527fd9
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-04-24 19:51:40 +0200

    bump version

--- TeraPlugin.cs
+++ TeraPlugin.cs
@@ -7,7 +7,7 @@ namespace TeraFinder
 {
     public class TeraPlugin : IPlugin
     {
-        public const string Version = "2.0.0";
+        public const string Version = "2.1.0";
         private bool UpdatePrompted = false;
 
         public string Name => nameof(TeraFinder);
