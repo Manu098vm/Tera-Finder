2f0355d6c2ddea5edfd728592200bf308de9c242
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-09-23 20:03:14 +0200

    Use inline namespace

--- TeraFinder.Core/Classes/ExtraMoves.cs
+++ TeraFinder.Core/Classes/ExtraMoves.cs
@@ -1,24 +1,23 @@
 ﻿using PKHeX.Core;
 
-namespace TeraFinder.Core
-{
-    public class ExtraMoves
-    {
-        public Move ExtraMove1;
-        public Move ExtraMove2;
-        public Move ExtraMove3;
-        public Move ExtraMove4;
-        public Move ExtraMove5;
-        public Move ExtraMove6;
+namespace TeraFinder.Core;
 
-        public ExtraMoves(ushort extra1 = 0, ushort extra2 = 0, ushort extra3 = 0, ushort extra4 = 0, ushort extra5 = 0, ushort extra6 = 0)
-        {
-            ExtraMove1 = (Move)extra1;
-            ExtraMove2 = (Move)extra2;
-            ExtraMove3 = (Move)extra3;
-            ExtraMove4 = (Move)extra4;
-            ExtraMove5 = (Move)extra5;
-            ExtraMove6 = (Move)extra6;
-        }
+public class ExtraMoves
+{
+    public Move ExtraMove1;
+    public Move ExtraMove2;
+    public Move ExtraMove3;
+    public Move ExtraMove4;
+    public Move ExtraMove5;
+    public Move ExtraMove6;
+
+    public ExtraMoves(ushort extra1 = 0, ushort extra2 = 0, ushort extra3 = 0, ushort extra4 = 0, ushort extra5 = 0, ushort extra6 = 0)
+    {
+        ExtraMove1 = (Move)extra1;
+        ExtraMove2 = (Move)extra2;
+        ExtraMove3 = (Move)extra3;
+        ExtraMove4 = (Move)extra4;
+        ExtraMove5 = (Move)extra5;
+        ExtraMove6 = (Move)extra6;
     }
 }
