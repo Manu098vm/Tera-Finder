8ac5ab01a77562d4ef55a872695dc49455c913c8
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-10-12 18:51:25 +0200

    Version update

--- TeraFinder.Plugins/TeraPlugin.cs
+++ TeraFinder.Plugins/TeraPlugin.cs
@@ -7,7 +7,7 @@ namespace TeraFinder.Plugins;
 
 public class TeraPlugin : IPlugin
 {
-    public const string Version = "2.8.0";
+    public const string Version = "2.8.1";
     private bool UpdatePrompted = false;
 
     public string Name => nameof(TeraFinder);
