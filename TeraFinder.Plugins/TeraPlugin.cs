4b6bd042043b0379963925bc15333832a7b0e804
Jonathan Herbert <foohyfooh@gmail.com>
2023-12-28 12:14:28 -0400

    Handle Outbreak Enable Block Being Dummy Block

--- TeraFinder.Plugins/TeraPlugin.cs
+++ TeraFinder.Plugins/TeraPlugin.cs
@@ -315,7 +315,7 @@ public class TeraPlugin : IPlugin
     public uint GetOutbreakEventIdentifier()
     {
         var enableBlock = SAV.Accessor.FindOrDefault(Blocks.KBCATOutbreakZonesPaldea.Key);
-        if (enableBlock.Type == SCTypeCode.Bool1)
+        if (enableBlock.Type == SCTypeCode.None || enableBlock.Type == SCTypeCode.Bool1)
             return 0;
         var pokeDataBlock = SAV.Accessor.FindOrDefault(Blocks.KBCATOutbreakPokeData.Key);
         var tablePokeData = FlatBufferConverter.DeserializeFrom<DeliveryOutbreakPokeDataArray>(pokeDataBlock.Data);
