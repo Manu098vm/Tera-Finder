b8987ea3d9e103aa02d2e86873f341490b8b4857
Manu <52102823+Manu098vm@users.noreply.github.com>
2024-05-10 04:35:45 +0200

    Add latest event legality; bump version

--- TeraFinder.Plugins/TeraPlugin.cs
+++ TeraFinder.Plugins/TeraPlugin.cs
@@ -9,7 +9,7 @@ namespace TeraFinder.Plugins;
 
 public class TeraPlugin : IPlugin
 {
-    public const string Version = "4.0.1";
+    public const string Version = "4.0.2";
     private bool UpdatePrompted = false;
 
     public string Name => nameof(TeraFinder);
